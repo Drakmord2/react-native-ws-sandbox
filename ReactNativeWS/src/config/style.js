8a59d0c0419f8cd9d6da1255548f3829dcb43563
Rubens Carneiro <rubenscarneiro@midiaseducativas.com.br>
2020-11-25 02:27:09 -0300

    Setting up App configurations

---
+++ ReactNativeWS/src/config/style.js
@@ -0,0 +1,19 @@
+const theme = {
+    light: {
+      background: '#ededed',
+      border: '#bdbdbd',
+      backgroundAlt: '#eaeaeb',
+      borderAlt: '#bdbdbd',
+      text: '#171717'
+    },
+    dark: {
+      background: '#2E3440',
+      border: '#575c66',
+      backgroundAlt: '#575c66',
+      borderAlt: '#2E3440',
+      text: '#ECEFF4'
+    }
+  };
+
+  export default theme;
+  
--- ReactNativeWS/src/index.js
+++ ReactNativeWS/src/index.js
@@ -6,109 +6,36 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
-  ScrollView,
   View,
   Text,
   StatusBar,
+  Switch
 } from 'react-native';
+import theme from './config/style';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [toggle, setToggle] = useState(false)
 
-const App: () => React$Node = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView style={{flex: 1, backgroundColor: theme.dark.background}}>
+        <View style={{marginTop: 25}}>
+          <Text style={{color: theme.dark.text, fontWeight: 'bold', fontSize: 30, marginLeft: 10}}>WS Sandbox</Text>
+        </View>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: "center"}}>
+          <Switch
+            value={toggle}
+            onValueChange={value => setToggle(value)}
+          />
+        </View>
+        
       </SafeAreaView>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
